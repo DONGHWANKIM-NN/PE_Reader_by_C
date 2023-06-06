f807197c2dd91504e08ed334f806e262b3ad2376
gbdngb12 <gbdngb12@naver.com>
2023-06-06 13:35:57 +0900

    bug fix

--- pe_reader_by_c.c
+++ pe_reader_by_c.c
@@ -11,30 +11,29 @@
 
 
 
-int main(int argc, const char** argv[]) {
+int main(int argc, const char **argv[]) {
 
-  //if (argc <= 1) {
-  //    printf("insert file\n");
-  //    exit(1);
-  //}   
-  
+  if (argc <= 1) {
+    printf("insert file\n");
+    exit(1);
+  }
+
   //@brief 파일 입출력을 위한 파일 포인터
   FILE *fp;
 
   //@brief 파일 open, 실패 시 프로그램을 종료
   fp = fopen(argv[1]/*프로그램 매개변수로 받은 파일 이름*/, "rb" /*바이너리 읽기 모드*/);
 
-
-
   if (fp == NULL) {
-      printf("파일 열기에 실패했습니다.\n");
-      exit(1);
+    printf("파일 열기에 실패했습니다.\n");
+    exit(1);
   }
 
-  
+
   //char list_msdosheader[50] = {"e_magic", "e_cblp", "e_cp", "e_cric", "e_cparhdr", "e_minalloc", "e_maxalloc", "e_ss", "e_sp", "e_csum", "e_ip", "e_cs", "e_lfarlc", "e_ovno", "e_res", "e_oemid", "e_oeminfo", "e_res2", "e_lfanew"};
 
-    int fpPosition = ftell(fp); // pe header를 읽는 데에 ms dos stub을 생략할 필요가 있기에, file offset을 pe header 바로 앞으로 e_lfanew를 이용해 바꿔주기 위한 변수 
+  int fpPosition =
+      ftell(fp); // pe header를 읽는 데에 ms dos stub을 생략할 필요가 있기에, file offset을 pe header 바로 앞으로 e_lfanew를 이용해 바꿔주기 위한 변수
 
 
   // MS DOS HEADER 읽고 출력  
@@ -73,65 +72,45 @@ int main(int argc, const char** argv[]) {
   //IMAGE DATA DIRECTORY 출력
   // IMAGE_OPTIONAL_HEADER64 optional_header; 이거 하드코딩 하다가 주석처리 해놓습니다.
   printf("IMAGE DATA DIRECTORY\n");
-  for(int i = 0; i+1 <= IMAGE_NUMBEROF_DIRECTORY_ENTRIES; i++){
-  printf("[%d]", i); //이러는 게 보기 좋아서 이렇게 둡니다.
-  printf("Virtual Address : %x\n", pe_header.OptionalHeader.DataDirectory[i].VirtualAddress);
-  printf("Size : %x\n\n", pe_header.OptionalHeader.DataDirectory[i].Size);
+  for (int i = 0; i + 1 <= IMAGE_NUMBEROF_DIRECTORY_ENTRIES; i++) {
+    printf("[%d]", i); //이러는 게 보기 좋아서 이렇게 둡니다.
+    printf("Virtual Address : %x\n", pe_header.OptionalHeader.DataDirectory[i].VirtualAddress);
+    printf("Size : %x\n\n", pe_header.OptionalHeader.DataDirectory[i].Size);
   }
 
   //print_imagedatadirectory(/*imgdata_directory*/optional_header.DataDirectory);
 
 
 
+  int number_of_sections = pe_header.FileHeader.NumberOfSections;
   //section header 읽기
-  IMAGE_SECTION_HEADER section_header;
-
-  int listfpPosition[7];
-
-  int sizeofrawdata[20];
+  IMAGE_SECTION_HEADER
+      *section_header = (IMAGE_SECTION_HEADER *) malloc(sizeof(IMAGE_SECTION_HEADER) * number_of_sections);
   //section header 출력
   printf("\nSECTION HEADER\n");
-  for(int i = 0; i+1<=pe_header.FileHeader.NumberOfSections; i++){
-    
-  listfpPosition[i] = section_header.PointerToRawData;
-
-  fread(&section_header, sizeof(section_header), 1, fp);
-  sizeofrawdata[i] = sizeof(section_header);
-  print_sectionheader(section_header);
+  for (int i = 0; i < number_of_sections; i++) {
+    fread(&section_header[i], sizeof(IMAGE_SECTION_HEADER), 1, fp);
+    print_sectionheader(section_header[i]);
+    printf("\n\n");
   }
 
+  for (int i = 0; i < number_of_sections; i++) {
+    printf("%s\n", section_header[i].Name);
+    fseek(fp, section_header[i].PointerToRawData, SEEK_SET);
+    char *section = (char *) malloc(sizeof(char) * section_header[i].SizeOfRawData);
+    fread(section, section_header[i].SizeOfRawData, 1, fp);
 
-
-  
-
-  //SECTION 읽겠습니다!! 근데 어떻게 읽으면 좋냐... 구조체도 없는데
-  // fpPosition = fseek(fp, section_header.PointerToRawData, SEEK_SET); // pointer to raw data를 이용해서 file offset을 section 앞으로 가져갈 거다.
-  
-  // for(int i = 0; i + 1 <= pe_header.FileHeader.NumberOfSections; i++){
-  // char section; // 긴 section을 담을 공간.
-
-  // fread(&section, section_header.SizeOfRawData, 1, fp);
-  // printf("%s\n", section_header.Name[i]);
-  // printf("%s\n\n\n", section);
-
-  // }
-  for(int i = 0; i+1 <= pe_header.FileHeader.NumberOfSections; i++){
-    fpPosition = listfpPosition[i];
-    fseek(fp, fpPosition, SEEK_SET);
-
-    char *section = (char *)malloc(sizeof(char)*section_header.SizeOfRawData);
-    fread(section, section_header.SizeOfRawData, 1, fp);
-
-    for(int j = 0; j <= section_header.SizeOfRawData; j++){
-      printf("%c", section[i]);
+    for (int j = 0; j < section_header[i].SizeOfRawData; j++) {
+      printf("%x ", (unsigned char)section[j]);
     }
-
+    printf("\n\n");
+    free(section);
   }
-  
 
 
 
-  
+
+
 
 
 
@@ -139,23 +118,17 @@ int main(int argc, const char** argv[]) {
 
   //@brief 파일 close. 실패시 exit
   if (fclose(fp) != NULL) {
-      printf("파일을 닫는 데 실패했습니다.\n");
-      exit(1);
+    printf("파일을 닫는 데 실패했습니다.\n");
+    exit(1);
   }
+
+  free(section_header);
   return 0;
 }
 
-
-
-
-
-
-
-
-void print_dosheader(IMAGE_DOS_HEADER dos_header)
-{
+void print_dosheader(IMAGE_DOS_HEADER dos_header) {
   printf("MS DOS HEADER\n");
-  printf("e_magic : %c%c\n", dos_header.e_magic&255, dos_header.e_magic>>8);
+  printf("e_magic : %c%c\n", dos_header.e_magic & 255, dos_header.e_magic >> 8);
   printf("e_cblp : %x\n", dos_header.e_cblp);
   printf("e_cp : %x\n", dos_header.e_cp);
   printf("e_crlc : %x\n", dos_header.e_crlc);
@@ -176,14 +149,12 @@ void print_dosheader(IMAGE_DOS_HEADER dos_header)
   printf("e_lfanew : %x\n\n\n\n", dos_header.e_lfanew);
 }
 
-void print_peheader(IMAGE_NT_HEADERS64 pe_header) 
-{
+void print_peheader(IMAGE_NT_HEADERS64 pe_header) {
   printf("PE HEADER\n");
-  printf("Signature : %c%c\n\n\n", pe_header.Signature&255, pe_header.Signature>>8);
+  printf("Signature : %c%c\n\n\n", pe_header.Signature & 255, pe_header.Signature >> 8);
 }
 
-
-void print_pefileheader(IMAGE_FILE_HEADER pe_file_header){
+void print_pefileheader(IMAGE_FILE_HEADER pe_file_header) {
   printf("PE FILE HEADER\n");
   printf("Machine : %x\n", pe_file_header.Machine);
   printf("Number of Sections : %x\n", pe_file_header.NumberOfSections);
@@ -194,8 +165,7 @@ void print_pefileheader(IMAGE_FILE_HEADER pe_file_header){
   printf("Characteristics : %x\n\n\n", pe_file_header.Characteristics);
 }
 
-
-void print_peoptionalheader(IMAGE_OPTIONAL_HEADER64 pe_optional_header){
+void print_peoptionalheader(IMAGE_OPTIONAL_HEADER64 pe_optional_header) {
   printf("PE OPTIONAL HEADER\n");
   printf("magic : %x\n", pe_optional_header.Magic);
   printf("Major Linker Version : %x\n", pe_optional_header.MajorLinkerVersion);
@@ -228,7 +198,7 @@ void print_peoptionalheader(IMAGE_OPTIONAL_HEADER64 pe_optional_header){
   printf("Number of RVA and Sizes : %x\n\n\n\n", pe_optional_header.NumberOfRvaAndSizes);
 }
 
-  
+
 // void print_imagedatadirectory(IMAGE_DATA_DIRECTORY imgdata_directory){
 //   int i = 1;
 //   while(i<=IMAGE_NUMBEROF_DIRECTORY_ENTRIES){
@@ -239,7 +209,7 @@ void print_peoptionalheader(IMAGE_OPTIONAL_HEADER64 pe_optional_header){
 // }
 
 
-void print_sectionheader(IMAGE_SECTION_HEADER section_header){
+void print_sectionheader(IMAGE_SECTION_HEADER section_header) {
   printf("Name : %s\n", section_header.Name);
   printf("Virtual size : %x\n", section_header.Misc.VirtualSize);
   printf("Virtual Address : %x\n", section_header.VirtualAddress);
